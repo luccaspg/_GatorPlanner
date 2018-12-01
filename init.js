965b4b1cadb2e2d8fc0d191dbed27d5d6504cb64
mirandacraghead <36489005+mirandacraghead@users.noreply.github.com>
2018-12-01 16:55:52 -0500

    12/1 changes

--- init.js
+++ init.js
@@ -4,3 +4,26 @@ function unhideL(){
 function unhideR(){
     document.getElementById('register').style = "display: block;";
 }
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+function display(value){
+    console.log(document.getElementById('a').option)
+}
