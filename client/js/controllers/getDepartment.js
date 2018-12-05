d469681fc742ca9b03f2280777859e191d0d4add
Luccas <luccaspg@gmail.com>
2018-12-05 00:32:17 -0500

    tree working, stuff deleted

--- client/js/controllers/getDepartment.js
+++ client/js/controllers/getDepartment.js
@@ -745,13 +745,111 @@ var departmentCodes = [
     ];
     
 function getDepartmentCode(){
+	var CODE;
     var value = document.getElementById('pickedDepartment').value;
     for (var i = 0; i < departmentCodes.length; i++){
 		console.log(departmentCodes[i]);
         if (departmentCodes[i].DESC == value){
 			var $scope = angular.element()
             console.log(departmentCodes[i].CODE);
-            return departmentCodes[i].CODE;
+			// return departmentCodes[i].CODE;
+			CODE = departmentCodes[i].CODE;
+			break;
         }
+	}
+	//Luccas:
+	//request to get the data and build the tree
+
+	
+	var url = "http://localhost:8080/course/" + CODE;
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url, true);
+
+	xhr.onload = function(){
+		var data = JSON.parse(this.response);
+		drawChart(data);
+	}
+	
+	xhr.send();
+}
+
+google.charts.load('current', {packages:["orgchart"]});
+      google.charts.setOnLoadCallback(drawChart);
+    
+    
+      function drawChart(jsonData) {
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Name');
+        data.addColumn('string', 'Manager');
+        data.addColumn('string', 'ToolTip');
+       filterPreReqs(jsonData);
+
+        // For each orgchart box, provide the name, manager, and tooltip to show.
+        var i;
+        for(i = 0; i < jsonData[0].length; i++){
+            
+          data.addRows([
+          [jsonData[0][i].code, jsonData[0][i].prerequisites, '']
+          ]);
+        }
+
+        // Create the chart.
+        var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));
+        // Draw the chart, setting the allowHtml option to true for the tooltips.
+        var options = {
+            allowHtml:true,
+            allowCollapse:true,
+            // nodeClass:"classNode"
+            
+            };
+        chart.draw(data, options);
+
+        //EVENT LISTENER FOR THE POPULATE
+        google.visualization.events.addListener(chart, 'select', popDescription);
+              
+function popDescription(){
+
+    
+    var selectedItem = chart.getSelection()[0];
+    if (selectedItem) {
+      var courseCode = data.getValue(selectedItem.row, 0);
+    //   alert(data.getValue(selectedItem.row, 3));
+
+    //CREATING POP UP
+    var popUp = document.createElement('div');
+    popUp.setAttribute('class', "popup");
+    popUp.textContent = courseCode;
+    var divsection = document.getElementById("description");
+    divsection.appendChild(popUp);
+    popUp.classList.toggle("show");
+    
+    // createDescription(topping);
     }
+
+}
+
+function createDescription(code){
+
+
+}
+
+      function filterPreReqs(jsonData){
+          var x;
+          for(i = 0; i < jsonData[0].length; i++){
+            jsonData[0][i].code.replace(/ /g, '');
+            jsonData[0][i].prerequisites = jsonData[0][i].prerequisites.replace(/ /g, '');
+
+            // jsonData[0][0].COURSES[i].prerequisites = jsonData[0][0].COURSES[i].prerequisites.replace(/(?![A-Z])./g, '');
+            var x = jsonData[0][i].prerequisites.search("Prereq:") + 7;
+              
+            jsonData[0][i].prerequisites = jsonData[0][i].prerequisites.slice(x,x+7);
+          }
+      }
+
+
+
+
+  
+
+
 }
